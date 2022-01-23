d4e9c5063643d2a8aba8debbd81ec491def6b31c
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-01-23 17:48:56 +0300

    fixing2

--- infra_deploy/psql/main.tf
+++ infra_deploy/psql/main.tf
@@ -8,13 +8,13 @@ resource "azurerm_postgresql_server" "az_psql" {
 
   sku_name     = "B_Gen5_2"
   capacity = "${var.pgsql_capacity[terraform.workspace]}"
-  tier     = "${var.pgsql_tier[terraform.workspace]}"
-  family   = "Gen5"
+  #tier     = "${var.pgsql_tier[terraform.workspace]}"
+  #family   = "Gen5"
 
   storage_mb            = "${var.pgsql_storage[terraform.workspace]}"
   backup_retention_days = "${var.pgsql_backup[terraform.workspace]}"
-  geo_redundant_backup_enabled = true
-  geo_redundant_backup  = "${var.pgsql_redundant_backup[terraform.workspace]}"
+  geo_redundant_backup_enabled = false
+  #geo_redundant_backup  = "${var.pgsql_redundant_backup[terraform.workspace]}"
   
   version                      = "9.6"
   ssl_enforcement              = "Enabled"
