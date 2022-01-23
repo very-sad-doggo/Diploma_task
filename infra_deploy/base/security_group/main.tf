9dae89d2be9685ed0aef3f349432bbedca1e405f
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-01-23 17:39:56 +0300

    fixes

--- infra_deploy/base/security_group/main.tf
+++ infra_deploy/base/security_group/main.tf
@@ -1,4 +1,4 @@
-resource azurerm_network_security_group "net_sec_group" {
+resource azurerm_network_security_group "security_group" {
   name                = "aks-${terraform.workspace}-nsg"
   location            = "${var.location}"
   resource_group_name = "${var.res_group_name}"
