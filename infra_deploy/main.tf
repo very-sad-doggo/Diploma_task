b3afb8603df7b716278a55956217b2b2278e220b
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-01-23 18:28:21 +0300

    fix

--- infra_deploy/main.tf
+++ infra_deploy/main.tf
@@ -27,7 +27,7 @@ module "vnet" {
 module "subnet" {
   source           = "./base/subnet"
   res_group_name   = "vladimir-ryadovoy-diploma"
-  net_sec_group_id = "${module.security_group.net_sec_group_id}"
+  #net_sec_group_id = "${module.security_group.net_sec_group_id}"
   vnet_name        = "${module.vnet.vnet_name}"
   subnet_prefixes  = "${var.subnet_prefixes}"
 }
