a44119899f3eff76fafa518225f902271f28a509
этот василий <92812823+very-sad-doggo@users.noreply.github.com>
2022-02-11 23:25:03 +0300

    prereq_scrip

--- infra_deploy/prerequisites.sh
+++ infra_deploy/prerequisites.sh
@@ -38,3 +38,24 @@ cat <<EOF > backend.tfvars
 EOF
 echo backend.tfvars
 cat backend.tfvars
+
+#!/bin/bash
+
+#Get AppOwner Tennant ID
+AZ_TENANT_ID=$(az ad sp list --display-name epm-rdsp-azure-devops --query "[].appOwnerTenantId" --out tsv)
+#Get subscription ID 
+SUBSCRIPTION_ID=$( az account list --query "[?isDefault].id" -o tsv )
+#Get Client ID
+SERVICE_PRINCIPAL_ID=$(az ad sp list --display-name epm-rdsp-azure-devops --query '[].appId' -o tsv)
+SERVICE_PRINCIPAL_OBJECT_ID=$(az ad sp list --display-name epm-rdsp-azure-devops --query '[].objectId' -o tsv)
+
+#prepare terraform.tfvars file
+cat <<EOF > terraform.tfvars
+    subscription_id = "$SUBSCRIPTION_ID"
+    client_id       = "$SERVICE_PRINCIPAL_ID"
+    client_secret   = "Wye7Q~JKsFwuLOw11euTt4EHupLWcLIVNyjoU"
+    tenant_id       = "$AZ_TENANT_ID"
+    pgsql_password   = "Vova&Gena42069"
+EOF
+echo terraform.tfvars
+cat terraform.tfvars
